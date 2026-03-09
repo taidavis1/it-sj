62ad5ac5bec5fe69b41a562875b11defd43f4aaa
taimongus <jacobpham82@gmail.com>
2026-03-08 22:30:20 -0700

    changes

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -4,7 +4,7 @@ import { Navbar } from "@/components/Navbar";
 import { StoreProvider } from "@/redux/StoreProvider";
 import Footer from "@/components/Footer";
 import FloatBtn from "@/components/FloatBtn";
-import "/node_modules/flag-icons/css/flag-icons.min.css";
+import "flag-icons/css/flag-icons.min.css";
 
 const fontSora = Sora({
 	subsets: ["latin"],
--- frontend/components/FloatBtn.js
+++ frontend/components/FloatBtn.js
@@ -5,9 +5,9 @@ import Link from 'next/link';
 const FloatBtn = () => {
     return (
         <div className=' fixed bottom-4 lg:bottom-12 lg:right-10 right-4 z-50'>
-            <select className="cursor-pointer appearance-none rounded-full bg-main/60 backdrop-blur-lg shadow-lg focus:outline-none text-white p-4 relative">
-                <option data-content='<span class="fi fis fi-jp"></span>' value="en"></option>
-                <option value="jpn"></option>
+            <select className="cursor-pointer text-2xl lg:text-4xl appearance-none rounded-full bg-main/60 backdrop-blur-lg shadow-lg text-center focus:outline-none text-white lg:w-16 lg:h-16 w-12 h-12 relative">
+                <option value="en">🇺🇸</option>
+                <option value="jpn">🇯🇵</option>
             </select>
         </div>
     )
